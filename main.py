bb65fd699b96adcc992868240314902569e57473
Nikita Yarantsev <narancev.work@gmail.com>
2025-11-18 15:31:05 +0300

    add url_for in routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,12 +7,19 @@ menu = ["Установка", "Первое приложение", "Обратн
 
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
 
 
 if __name__ == "__main__":
